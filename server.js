4c6511fdbc22da71bb3e905e0f5c9ced74fe793b
goodtimingtrading-byte <goodtimingtrading@gmail.com>
2026-06-04 00:04:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,31 +2,25 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(express.static(path.join(__dirname, '/')));
 
-let rooms = { 'lobby': { players: {} } };
+let rooms = { 'room1': { hands: {}, hp: { 'p1': 100, 'p2': 100 } } };
 
 io.on('connection', (socket) => {
-    socket.on('joinRoom', (roomId) => {
-        socket.join(roomId);
-        rooms[roomId].players[socket.id] = { id: socket.id };
-        io.to(roomId).emit('updateRoom', { players: Object.keys(rooms[roomId].players) });
-    });
-
+    socket.on('joinRoom', (room) => socket.join(room));
     socket.on('submitHands', ({ roomId, hands }) => {
-        if (!rooms[roomId]) rooms[roomId] = { hands: {}, hp: { p1: 100, p2: 100 } };
         rooms[roomId].hands[socket.id] = hands[0];
         if (Object.keys(rooms[roomId].hands).length === 2) {
-            // 簡易勝負邏輯處理
-            io.to(roomId).emit('battleResult', { status: '回合結束' });
+            const ids = Object.keys(rooms[roomId].hands);
+            const result = { winner: 'Draw', newHp: rooms[roomId].hp };
+            io.to(roomId).emit('battleResult', result);
             rooms[roomId].hands = {};
         }
     });
 });
 
-server.listen(process.env.PORT || 3000, () => console.log('Server running...'));
+server.listen(process.env.PORT || 3000);
